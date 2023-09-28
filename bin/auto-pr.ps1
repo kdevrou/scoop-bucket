fcd5567aba8f1862a4c0520c641dbbce7b3ebb4e
Kevin DeVrou <kevin.devrou@gmail.com>
2023-09-27 23:38:41 -0400

    Update auto-pr.ps1
    correct repo name

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "devrouk/scoop-bucket:main"
+    [String]$upstream = "kdevrou/scoop-bucket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
